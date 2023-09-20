f07392f0fa9d733b8584f566b97425f636652277
Vishal <105773618+viishalll@users.noreply.github.com>
2023-09-20 19:00:23 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,36 @@
+<footer class="footer">
+  <div class="container">
+    <div class="row">
+      <div class="col-4 col-md-2   align-self-center">
+        <img class="align-text-center img-fluid" src="assets/img/footer-logo.png" />
+      </div>
+      <div class="offset-sm-2 col-5 ">
+        <h5>Our Address</h5>
+        <address>
+          Bengaluru,Karnataka
+        </address>
+      </div>
+      <div class="col-12 col-md-3 align-self-center">
+      </div>
+    </div>
+    <hr>
+  </div>
+</footer>
+
+<script src="https://cdn.staticfile.org/jquery/3.6.3/jquery.min.js"></script>
+
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.js"></script>
+
+
+<script src="https://cdn.staticfile.org/markdown-it/13.0.1/markdown-it.min.js"></script>
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+  integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+
+
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+  integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+
+<script src="https://use.fontawesome.com/ee1c3da296.js"></script>
